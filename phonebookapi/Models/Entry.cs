ff5db096b6dcf905d393fb5c0b426d4e2776d7aa
David <david.francis@travelstart.com>
2020-04-25 13:47:33 +0200

    Added phone book models

---
+++ phonebookapi/Models/Entry.cs
@@ -0,0 +1,9 @@
+﻿namespace phonebookapi.Models
+{
+    public class Entry
+    {
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ phonebookapi/Models/PhoneBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace phonebookapi.Models
+{
+    public class PhoneBook
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public List<Entry> Entries { get; set; }   
+    }
+}
